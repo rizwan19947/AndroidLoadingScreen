7ea4705a7fb1285b73bad7bc4ab0569a4d8032a0
Rizwan Muhammad Syed <expectp@gmail.com>
2020-08-17 14:18:35 +0500

    Only animation works, it is not customizable

---
+++ app/src/main/java/com/example/finalassignment/AnimationView.kt
@@ -0,0 +1,49 @@
+package com.example.finalassignment
+import android.content.Context
+import android.animation.ObjectAnimator
+import android.content.res.Resources
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.ClipDrawable
+import android.graphics.drawable.Drawable
+import android.view.View
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import java.util.logging.Handler
+
+class AnimationView(context: Context) : RelativeLayout(context){
+
+
+
+
+
+
+
+
+    fun setViewSize(width : Int, height: Int){
+
+
+    }
+
+
+    fun setFillColor(color: String){
+
+    }
+
+    fun doAnimation (){
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
--- app/src/main/java/com/example/finalassignment/MainActivity.kt
+++ app/src/main/java/com/example/finalassignment/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.finalassignment
 
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 
 /*TODO
@@ -16,8 +23,59 @@ import android.os.Bundle
 
 */
 class MainActivity : AppCompatActivity() {
+
+    lateinit var avdc: AnimatedVectorDrawableCompat
+    lateinit var avd: AnimatedVectorDrawable
+    lateinit var animationView : AnimationView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+//        animationView = AnimationView(this)
+
+
+
+
     }
+
+
+
+
+
+
+    fun animate(view: View) {
+
+
+        var imageView:ImageView = imageView1
+        var drawable: Drawable = imageView.drawable
+//        animationView.setViewSize(300,300)
+
+
+
+
+        if(drawable is AnimatedVectorDrawableCompat){
+            this.avdc = (drawable)
+            avdc.start()
+        }
+        else if (drawable is AnimatedVectorDrawable){
+            this.avd = (drawable)
+            avd.start()
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
 }
